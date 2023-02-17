38c4dcdc6ce9ff6ad17795d2b3ca359cefb4642a
2641472602@qq.com <2641472602@qq.com>
2023-02-17 14:23:22 +0800

    初始化

---
+++ docs/.vuepress/.temp/internal/pagesComponents.js
@@ -0,0 +1,8 @@
+import { defineAsyncComponent } from 'vue'
+
+export const pagesComponents = {
+  // path: /
+  "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"C:/新建文件夹/vuePress/docs/.vuepress/.temp/pages/index.html.vue")),
+  // path: /404.html
+  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"C:/新建文件夹/vuePress/docs/.vuepress/.temp/pages/404.html.vue")),
+}
---
+++ docs/.vuepress/.temp/internal/pagesData.js
@@ -0,0 +1,6 @@
+export const pagesData = {
+  // path: /
+  "v-8daa1a0e": () => import(/* webpackChunkName: "v-8daa1a0e" */"C:/新建文件夹/vuePress/docs/.vuepress/.temp/pages/index.html.js").then(({ data }) => data),
+  // path: /404.html
+  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"C:/新建文件夹/vuePress/docs/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
+}
